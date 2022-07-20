f323cf4ca816977b88395f4627cc334d2407cca1
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-07-20 17:06:03 +0200

    create table

--- public/js/front.js
+++ public/js/front.js
@@ -12562,7 +12562,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\corso-boolean-php\laravel-auth\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\corso-boolean-php\laravel-one-to-many\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
